8e2bce2f51a31728e105860c7fd63ad9727b7dbe
Puttipongpn <fampoom1@gmail.com>
2023-07-14 17:14:11 +0700

    update

--- app/routes/index.js
+++ app/routes/index.js
@@ -82,6 +82,26 @@ router.get('/', ifNotLoggedin, (req, res, next) => {
 
 });// END OF ROOT PAGE
 
+router.get('/respond', ifNotLoggedin, (req, res, next) => {
+    dbConnection.execute("SELECT * FROM `news` WHERE ?", [req.session.userID])
+        .then(([rows]) => {
+            if (rows[0].role === "USER") {
+                res.render('user_page/request_official', {
+                    users: rows,
+                    name: rows[0].name,
+                    role: rows[0].role,
+                    user_name: rows[0].user_name,
+                    email: rows[0].email,
+                    profile_image: req.session.profile_image,
+                });
+            } else if (rows[0].role === "ADMIN") {
+                res.render('404page')
+            } else {
+                res.redirect('/')
+            }
+        });
+});
+
 
 //USER---------------------------------------------------------------------------------------
 
@@ -166,6 +186,25 @@ router.post('/addrequest_official/:id', ifNotLoggedin, (req, res) => {
 //             res.redirect('404page');
 //         });
 // });
+router.get('/profile', ifNotLoggedin, (req, res, next) => {
+    dbConnection.execute("SELECT * FROM users LEFT JOIN news ON users.id = news.user_id LEFT JOIN topic ON news.topic_id = topic.topic_id LEFT JOIN news_type ON news.news_type_id = news_type.news_type_id WHERE users.id = 3;", [req.session.userID])
+        .then(([rows]) => {
+            if (rows[0].role === "USER") {
+                res.render('user_page/request_official', {
+                    users: rows,
+                    name: rows[0].name,
+                    role: rows[0].role,
+                    user_name: rows[0].user_name,
+                    email: rows[0].email,
+                    profile_image: req.session.profile_image,
+                });
+            } else if (rows[0].role === "ADMIN") {
+                res.render('404page')
+            } else {
+                res.redirect('/')
+            }
+        });
+});
 
 const multer = require('multer');
 const storage = multer.diskStorage({
