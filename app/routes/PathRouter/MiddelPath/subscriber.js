e5c75e6870f5713dd682ec4a99243a9490820a60
Puttipongpn <fampoom1@gmail.com>
2023-09-25 14:41:26 +0700

    update subscribe.js

--- app/routes/PathRouter/MiddelPath/subscriber.js
+++ app/routes/PathRouter/MiddelPath/subscriber.js
@@ -6,19 +6,26 @@ const { ifLoggedin } = require('../../loginRouter/ifLoggedin');
 const { ifNotLoggedin } = require('../../loginRouter/ifNotLoggedin');
 router.use(express.urlencoded({ extended: false }));
 
-router.get('/', (req, res) => {
-    // ดึงข้อมูลจากฐานข้อมูล
-    dbConnection.execute("SELECT * FROM news")
-        .then(([rows]) => { 
-            res.render('center/subscriber',{
-                sub: rows[0],
-                header:req.session.header
+router.get('/', async (req, res) => {
+    try {
+        // ดึงข้อมูลจากฐานข้อมูล
+        user_id = req.session.userID
+        news = await dbConnection.execute("SELECT users.* , news.* FROM news LEFT JOIN users ON news.user_id = users.id LEFT JOIN subscribe ON subscribe.sub_user_id = news.user_id LEFT JOIN approve_news ON approve_news.news_id = news.news_id WHERE approve_news.status_id = 2 AND subscribe.user_id = ?",[user_id])
+        dbConnection.execute("SELECT * FROM `subscribe` LEFT JOIN users ON subscribe.sub_user_id = users.id WHERE user_id = ?", [user_id])
+            .then(([rows]) => {
+                res.render('center/subscriber', {
+                    news : news[0],
+                    subHome: rows,
+                    header: req.session.header
+                })
             })
-        })
-        .catch(err => {
-            console.error(err);
-            res.status(500).json({ error: 'Internal Server Error' });
-        });
+            .catch(err => {
+                console.error(err);
+                res.status(500).json({ error: 'Internal Server Error' });
+            });
+    } catch (error) {
+
+    }
 });
 
 router.post('/:id', ifNotLoggedin, (req, res, next) => {
@@ -41,7 +48,7 @@ router.delete('/:id', ifNotLoggedin, (req, res, next) => {
         sub_user_id: req.params.id,
         user_id: req.session.userID
     };
-    dbConnection.query("DELETE FROM `subscribe` WHERE `sub_user_id` = ? AND `user_id` = ?; ", [newsDataWithUserId.sub_user_id,newsDataWithUserId.user_id])
+    dbConnection.query("DELETE FROM `subscribe` WHERE `sub_user_id` = ? AND `user_id` = ?; ", [newsDataWithUserId.sub_user_id, newsDataWithUserId.user_id])
         .then(([rows]) => {
             res.json({ message: 'บันทึกสำเร็จ' });
         })
