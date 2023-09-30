6ede34c7dcf5fd231c9e2c9d4ece755848a01bd1
sudarat9 <sudarat76692@gmail.com>
2023-09-30 15:30:13 +0700

    table หน้า apporove+history

--- app/routes/connect.js
+++ app/routes/connect.js
@@ -3,7 +3,7 @@ const dbConnection = mysql.createPool({
     host     : 'localhost', // MYSQL HOST NAME
     user     : 'root', // MYSQL USERNAME
     password : '', // MYSQL PASSWORD
-    database : 'sut_news' // MYSQL DB NAME
+    database : 'new_schema' // MYSQL DB NAME
 }).promise();
 
 module.exports = dbConnection;
