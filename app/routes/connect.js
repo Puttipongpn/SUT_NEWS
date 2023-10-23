a925fe5146aebba0f03dd02094aac92a9f3dd276
Puttipongpn <fampoom1@gmail.com>
2023-10-24 00:05:11 +0700

    แก้ไขการกันสิทธิ์ผิดพลาด

--- app/routes/connect.js
+++ app/routes/connect.js
@@ -3,7 +3,7 @@ const dbConnection = mysql.createPool({
     host     : 'localhost', // MYSQL HOST NAME
     user     : 'root', // MYSQL USERNAME
     password : '', // MYSQL PASSWORD
-    database : 'project_s' // MYSQL DB NAME
+    database : 'sut_new_2' // MYSQL DB NAME
 }).promise();
 
 module.exports = dbConnection;
